63df9704411f6e302cee69f9303a755fa5d4c5b0
RizkiHamdanii <officialkyy17@gmail.com>
2026-03-13 13:49:04 +0700

    Update penyimpanan file ke dalam database jika sudah direkrut dari pelamar ke karyawan

--- app/Models/Applicant.php
+++ app/Models/Applicant.php
@@ -5,33 +5,15 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 
+
 class Applicant extends Model
 {
-
     protected $table = 'applicants';
 
     protected $fillable = [
-        'nama_lengkap',
-        'tempat_lahir',
-        'tanggal_lahir',
-        'jenis_kelamin',
-        'pendidikan_terakhir',
-        'alamat',
-        'nomor_hp',
-        'cv',
-        'pas_foto',
-        'transkrip_nilai',
-        'ktp',
-        'ijazah',
-        'kartu_bpjs',
-        'suket_pengalaman_kerja',
-        'daftar_riwayat_hidup'
+        'nama_lengkap', 'tempat_lahir', 'tanggal_lahir', 'jenis_kelamin', 
+        'pendidikan_terakhir', 'alamat', 'nomor_hp', 'cv', 'pas_foto', 
+        'transkrip_nilai', 'ktp', 'ijazah', 'kartu_bpjs', 
+        'suket_pengalaman_kerja', 'daftar_riwayat_hidup'
     ];
-
-    public function jobPositions()
-    {
-        return $this->belongsToMany(JobPosition::class, 'job_position_pelamar')
-        ->withTimestamps();
-    }
-
 }
